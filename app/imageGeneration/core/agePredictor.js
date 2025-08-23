2c0ac1b8760c0f1728269900716809d1ca8ab359
yash-vamja <vamjayash532@gmail.com>
2025-08-23 16:23:12 +0530

    age predic change

--- app/imageGeneration/core/agePredictor.js
+++ app/imageGeneration/core/agePredictor.js
@@ -2,15 +2,23 @@ const ResponseModal = require('../../../handler/http/ResponseModal')
 const authDao = require('./dao')
 
 
-const agePredictor = async function (userid, uploadimage, Predict_age, genrater_Img, transactionId)
- {
+const agePredictor = async function (userid, uploadimage, Predict_age, genrater_Img, transactionId) {
     try {
-        let res = await authDao.agePredictor_insert(userid, uploadimage, Predict_age, genrater_Img, transactionId);
+        const result = await authDao.agePredictor_insert(userid, uploadimage, Predict_age, genrater_Img, transactionId);
+        if (!result || result.rowCount === 0) {
+            return new ResponseModal()
+                .setStatus('error')
+                .setStatusCode(400)
+                .setMessage('Insert failed');
+        }
         return new ResponseModal()
             .setStatus("success")
-            .setStatusCode(201)
-            .setData(res.rows[0])
-            .setMessage("Age prediction recorded successfully");
+            .setStatusCode(200)
+            .setMessage("Age prediction recorded successfully")
+            .setData({
+               agepredic: Predict_age,
+               file:uploadimage,
+            });
     } catch (error) {
         return new ResponseModal()
             .setStatus("error")
