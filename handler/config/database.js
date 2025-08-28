6a04dceb00710098c1b96d1442d7a5afcb686b43
Abhi Vagadiya <your-email@example.com>
2025-08-28 17:38:22 +0530

    hh

--- handler/config/database.js
+++ handler/config/database.js
@@ -1,6 +1,6 @@
 const database = {
     local: {
-        host: "dpg-d2nv5ajipnbc73d08m50-a.singapore-postgres.render.com",
+        host: "https://dpg-d2nv5ajipnbc73d08m50-a.singapore-postgres.render.com",
         user: "magicshot_user",
         password: "NiRJmLqMHSTb4PsPbMGIxaJ1GXupyva9",
         database: "magicshot",
