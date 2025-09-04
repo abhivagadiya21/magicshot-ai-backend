6ef02ce7f9782de859bdc17d41d4192039690252
yash-vamja <vamjayash532@gmail.com>
2025-09-04 17:28:45 +0530

    cors add origin *

--- app.js
+++ app.js
@@ -12,6 +12,9 @@ var app = express();
 
 app.use(logger('dev'));
 app.use(cors());
+app.use(cors({
+    origin: '*'
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
