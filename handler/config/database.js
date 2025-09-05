cbf07a71145125c7690be0aa494bb0663a88e600
Abhi Vagadiya <your-email@example.com>
2025-09-05 11:16:03 +0530

    Update database configuration to include SSL settings for secure connections

--- handler/config/database.js
+++ handler/config/database.js
@@ -1,11 +1,14 @@
 const database = {
     local: {
-        host: "dpg-d2nv5ajipnbc73d08m50-a",
+        host: "dpg-d2nv5ajipnbc73d08m50-a.singapore-postgres.render.com",
         user: "magicshot_user",
         password: "NiRJmLqMHSTb4PsPbMGIxaJ1GXupyva9",
         database: "magicshot",
         port: 5432,
-        idleTimeoutMillis: 3000
+        idleTimeoutMillis: 3000,
+        ssl: {
+            rejectUnauthorized: true, // needed if you're using self-signed certs (like on Heroku, Render, Neon, etc.)
+        },
     }
 
     // local: {
